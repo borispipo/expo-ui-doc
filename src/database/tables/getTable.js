f368a6d21dc16baf8a84de2a2a82c45b19288d29
LAPTOP-GA9EKJT2\BORIS <borispipo@gmail.com>
2024-03-17 19:59:39 +0100

    Update du package expo-ui à la dernière version

--- src/database/tables/getTable.js
+++ src/database/tables/getTable.js
@@ -1,8 +1,13 @@
 
-  export default function(tableName){
+/*****
+   le contenu de cette fonction peut être généré automatiquement via les commandes suivantes (étant dans le repertoire de l'application)
+       npm run generate-getTable | npx @fto-consult/expo-ui generate-getTable. Notons que le script generate-getTable est définit comme étant l'un des scripts du package.json de l'application
+   @param {string} tableName, le nom de la table data
+   @return {object | null}, table, l'objet table associé 
+*/
+export default function(tableName){
   	if(!tableName || typeof tableName !=="string") return null;
   	tableName = tableName.toUpperCase().trim();
   	if(tableName === "VCARD"){return require("./vCard").default;}
 	return null;
-  }
-              
+}
